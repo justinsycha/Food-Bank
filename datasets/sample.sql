d624f12268940c09cc9a0974dfd68f8d4310b77b
justinsycha <justin.sycha@bc-advisory.de>
2023-02-01 09:46:19 +0100

    Sample Data for QA

---
+++ datasets/sample.sql
@@ -0,0 +1,27 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Magic Mushrooms','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'2023-02-26','Refrigerated','Champignons','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'2023-02-28','Refrigerated','Austernpilze','1');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'2023-02-20T11:00:00.000+0000','Scheduled','1');
+COMMIT;
